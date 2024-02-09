7653872fc048ab9aabfcfd77435ec075bcbe7f4b
Greg Martin <greg.l.martin@gmail.com>
2024-02-09 11:14:21 -0500

    Updated the code to make the Sqitch target dynamic. This will help with programatic testing.

--- docker/run_migrations.sh
+++ docker/run_migrations.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Make the target dynamic https://sqitch.org/docs/manual/sqitch-deploy/
+
+: "${DB_NAME:="postgres"}"
+: "${DB_PORT:="5432"}"
+: "${DB_HOST:="postgres"}"
+: "${DB_USERNAME:="postgres"}"
 : "${DB_PASSWORD:="postgres"}"
-: "${MIGRATION_ACTION:="sqitch deploy"}"
+: "${MIGRATION_ACTION:="sqitch deploy --target db:pg://${DB_USERNAME}@${DB_HOST}:${DB_PORT}/${DB_NAME}"}"
 
 set -e
 
